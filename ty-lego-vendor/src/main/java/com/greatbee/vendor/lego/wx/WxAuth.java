412a4e4d9a9b883a7acf3e1315e6fe73c00af9f4
CarlChen <iamcarlchen@gmail.com>
2024-04-08 11:08:53 +0800

    ty-lego-vendor的package name改为com.greatbee.vendor

---
+++ ty-lego-vendor/src/main/java/com/greatbee/vendor/lego/wx/WxAuth.java
@@ -0,0 +1,176 @@
+package com.greatbee.vendor.lego.wx;
+
+import com.alibaba.fastjson.JSONObject;
+import com.greatbee.base.bean.DBException;
+import com.greatbee.base.util.Charset;
+import com.greatbee.base.util.StringUtil;
+import com.greatbee.db.ExceptionCode;
+import com.greatbee.api.bean.server.InputField;
+import com.greatbee.api.lego.Input;
+import com.greatbee.api.lego.Lego;
+import com.greatbee.api.lego.LegoException;
+import com.greatbee.core.lego.util.LegoUtil;
+import com.greatbee.db.util.HttpClientUtil;
+import org.apache.log4j.Logger;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * SelectCustomSQL
+ *
+ * 微信授权（登录、注册） lego
+ *
+ * @author xiaobc
+ * @date 18/6/21
+ */
+public abstract class WxAuth implements ExceptionCode, Lego{
+    private static final Logger logger = Logger.getLogger(WxAuth.class);
+
+    protected static final long ERROR_LEGO_WX_Params_Null = 300022L;
+
+    protected static final long ERROR_LEFO_WX_Sign_Error = 300031L;
+
+    private static final String WX_Open_Host="https://open.weixin.qq.com";
+    private static final String WX_Open_Api_Host = "https://api.weixin.qq.com";
+
+    protected static final String Input_Key_Wx_Open_App_Id = "openAppId";//微信公众号appID（公众号的唯一标识）
+    protected static final String Input_Key_WX_Open_Secret = "openSecret";//微信secret
+
+
+
+    /**
+     * 构建tpl模板需要的请求参数
+     * @param input
+     * @return
+     * @throws LegoException
+     */
+    protected Map buildTplParams(Input input) throws LegoException {
+        java.util.List ifs = input.getInputFields();
+        Map<String,Object> params = LegoUtil.buildTPLParams(input.getRequest(),null,null,input);
+        Iterator result = ifs.iterator();
+        while(result.hasNext()) {
+            InputField _if = (InputField)result.next();
+            params.put(_if.getFieldName(), _if.getFieldValue());
+        }
+        return params;
+    }
+
+    /**
+     * 获取授权authUrl
+     *  wxAuthUseAgent 使用中间代理跳转（解决微信授权域名只能配置一个的问题）
+     *
+     *  不用代理地址：https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
+     *  用代理地址：http://www.abc.com/xxx/get-weixin-code.html?appid=XXXX&scope=snsapi_base&state=hello-world&redirect_uri=http%3A%2F%2Fwww.xyz.com%2Fhello-world.html
+     *
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    protected String getAuthUrl(String wxAuthUseAgent,String appId,String redirectUrl,String scope,String state) throws UnsupportedEncodingException {
+        StringBuilder urlBuilder = new StringBuilder();
+        if(StringUtil.isValid(wxAuthUseAgent)){
+            urlBuilder.append(wxAuthUseAgent);
+        }else{
+            urlBuilder.append(WX_Open_Host);
+            urlBuilder.append("/connect/oauth2/authorize");
+        }
+        urlBuilder.append("?");
+        urlBuilder.append("appid=").append(appId);
+        urlBuilder.append("&redirect_uri=").append(URLEncoder.encode(redirectUrl, Charset.UTF_8));
+        urlBuilder.append("&response_type=code");
+        urlBuilder.append("&scope=").append(scope);
+        urlBuilder.append("&state=").append(state);
+        urlBuilder.append("#wechat_redirect");
+        return urlBuilder.toString();
+    }
+
+
+    /**
+     * 获取openid 的url
+     * @return
+     */
+    protected String getOpenIdUrl(String code,String appId,String openSecret) {
+        StringBuilder urlBuilder = new StringBuilder(WX_Open_Api_Host);
+        urlBuilder.append("/sns/oauth2/access_token?");
+        urlBuilder.append("appid=").append(appId);
+        urlBuilder.append("&secret=").append(openSecret);
+        urlBuilder.append("&code=").append(code);
+        urlBuilder.append("&grant_type=authorization_code");
+        return urlBuilder.toString();
+    }
+
+    /**
+     * 获取微信 open详情 包括授权access_token  openId 等信息
+     *
+       {
+         "access_token":"ACCESS_TOKEN",
+         "expires_in":7200,
+         "refresh_token":"REFRESH_TOKEN",
+         "openid":"OPENID",
+         "scope":"SCOPE"
+        }
+     * @param code
+     * @param appId
+     * @param openSecret
+     * @return
+     * @throws DBException
+     */
+    protected JSONObject getOpenInfo(String code,String appId, String openSecret) throws DBException {
+        String url = this.getOpenIdUrl(code,appId,openSecret);
+        logger.info("[WxAuth][getOpenId][getOpenUrl] url=" + url);
+        String httpResponse = HttpClientUtil.get(url,null).getResponseBody();
+        if (StringUtil.isInvalid(httpResponse)) {
+            logger.error("[WxAuth][getOpenId][error] httpResponse is invalid!");
+        }
+        logger.info("[WxAuth][getOpenId][info] httpResponse=" + httpResponse);
+        JSONObject jsonObject = JSONObject.parseObject(httpResponse);
+        return  jsonObject;
+    }
+    /**
+     * 获取openId
+     * @param code
+     * @return
+     * @throws DBException
+     */
+    protected String getOpenId(String code,String appId,String openSecret) throws DBException {
+        JSONObject jsonObject = getOpenInfo(code, appId, openSecret);
+        if (jsonObject.containsKey("openid")) {
+            return jsonObject.getString("openid");
+        }
+        return null;
+    }
+
+    /**
+     * 根据access_token 和 openid 获取用户信息
+     * @return
+     */
+    protected JSONObject getAuthUserInfo(String code,String appId,String openSecret) throws DBException {
+        JSONObject tokenOpenId = getOpenInfo(code,appId,openSecret);
+        String access_token = null;
+        String openId = null;
+        if (tokenOpenId.containsKey("access_token")) {
+            access_token = tokenOpenId.getString("access_token");
+        }
+        if(tokenOpenId.containsKey("openid")){
+            openId = tokenOpenId.getString("openid");
+        }
+
+        StringBuilder url = new StringBuilder(WX_Open_Api_Host);
+        url.append("/sns/userinfo?");
+        url.append("access_token="+access_token);
+        url.append("&openid="+openId);
+        url.append("&lang=zh_CN");
+
+        String httpResponse = HttpClientUtil.get(url.toString(),null).getResponseBody();
+        if (StringUtil.isInvalid(httpResponse)) {
+            logger.error("[WxAuth][getAuthUserInfo][error] httpResponse is invalid!");
+            return null;
+        }
+        JSONObject userinfo = JSONObject.parseObject(httpResponse);
+        return userinfo;
+    }
+
+
+}
---
+++ ty-lego-vendor/src/main/java/com/greatbee/vendor/utils/VendorExceptionCode.java
@@ -0,0 +1,43 @@
+package com.greatbee.vendor.utils;
+
+/**
+ * VendorExceptionCode vendor的所有异常code  后面vendr的异常code 都在这里维护
+ *
+ * @author xiaobc
+ * @date 18/7/31
+ */
+public class VendorExceptionCode {
+
+
+    public static final long Lego_Error_Index_data_Null = 300070L;
+    public static final long Lego_Error_CascadeAdd_Create_Field_Null = 300071L;
+
+    public static final long Lego_Error_WX_Upload_Error = 300201L;
+    public static final long Lego_Error_WX_Push_Error = 300202L;
+
+    //微信小程序
+    public static final long Lego_Error_WX_Mp_Appid_Null = 300203L;
+    public static final long Lego_Error_WX_Mp_Appsecret_Null = 300203L;
+    public static final long Lego_Error_WX_Mp_Generate_Error = 300204L;
+
+    //lego
+    public static final long Lego_Error_Mokelay_Param_Null = 300301L;
+    public static final long Lego_Error_Weidian_Post_Error = 300302L;
+    public static final long Lego_Error_Weidian_Auth_Error = 300303L;
+    public static final long Lego_Error_Weidian_No_Product_Need_Update = 300304L;
+    public static final long Lego_Error_Auto_Increment_FieldName_Null = 300305L;
+    public static final long Lego_Error_Web_Service_Config_Param_Null = 300306L;
+    public static final long Lego_Error_Xml_String_Formatting_Error = 300307L;
+    public static final long Lego_Error_Xml_String_Null = 300308L;
+    public static final long Lego_Error_Xml_Data_Null = 300309L;
+    public static final long Lego_Error_Fy_Static_3DES_Null = 300310L;
+    public static final long Lego_Error_Fy_RSA_Public_Key_Null = 300311L;
+    public static final long Lego_Error_Fy_Customer_Code_Null = 300312L;
+    public static final long Lego_Error_Fy_Customer_RSA_Private_Key_Null = 300313L;
+    public static final long Lego_Error_Fy_Web_Service_Error = 300314L;
+    public static final long Lego_Error_Fy_RSA_Key_Error = 300315L;
+    public static final long Lego_Error_Calculate_Tpl_Null = 300316L;
+    public static final long Lego_Error_Calculate_Error = 300317L;
+
+
+}
